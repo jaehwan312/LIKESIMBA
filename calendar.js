f64a7786d499856864c51588194a9ad9566e7c8a
ChangSIkKaang <93307009+ChangSIkKaang@users.noreply.github.com>
2021-12-02 12:49:12 +0900

    Add files via upload

---
+++ calendar.js
@@ -0,0 +1,34 @@
+const date = new Date();
+
+const monthDays = document.querySelector('.days')
+
+// const lastDay = new Date(date.getFullYear(), date.getMonth(), + 1,0).getDate();
+
+// console.log(lastDay)
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "January",
+    "December",
+];
+
+document.querySelector('.date h1').innerHTML = months[date.getMonth()];
+
+document.querySelector('.date p').innerHTML = date.toDateString();
+
+let days = '';
+
+for (let i = 1; i <= 31; i++) {
+    days += '<div>${i}</div>';
+}
+
+monthDays.innerHTML = days;
