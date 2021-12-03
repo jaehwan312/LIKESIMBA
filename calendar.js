b358d93f17dea60396ee0c7fea3fd6a834e203f1
ChangSIkKaang <93307009+ChangSIkKaang@users.noreply.github.com>
2021-12-03 13:57:59 +0900

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,10 +1,21 @@
 const date = new Date();
 
-const monthDays = document.querySelector('.days')
+const renderCalendar = () => {
+    date.setDate(1);
 
-// const lastDay = new Date(date.getFullYear(), date.getMonth(), + 1,0).getDate();
+console.log(date.getDate());
 
-// console.log(lastDay)
+const monthDays = document.querySelector('.days');
+
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1,0).getDate();
+
+const prevLastday = new Date(date.getFullYear(), date.getMonth(),0).getDate();
+
+const firstDayIndex = date.getDay();
+
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1,0).getDay();
+
+const nextDays = 7 - lastDayIndex - 1;
 
 const months = [
     "January",
@@ -17,7 +28,7 @@ const months = [
     "August",
     "September",
     "October",
-    "January",
+    "November",
     "December",
 ];
 
@@ -25,10 +36,35 @@ document.querySelector('.date h1').innerHTML = months[date.getMonth()];
 
 document.querySelector('.date p').innerHTML = date.toDateString();
 
-let days = '';
+let days = "";
 
-for (let i = 1; i <= 31; i++) {
-    days += '<div>${i}</div>';
+for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastday - x}</div>`;
 }
 
-monthDays.innerHTML = days;
+for (let i=1; i<=lastDay; i++) {
+    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+    days += `<div class="today">${i}</div>`;
+    } else {
+    days += `<div>${i}</div>`;
+    }
+}
+
+for (let j=1; j<=nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+}
+
+}
+
+document.querySelector('.prev').addEventListener('click',() => {
+    date.setMonth(date.getMonth() - 1);    
+    renderCalendar();
+});
+
+document.querySelector('.next').addEventListener('click',() => {
+    date.setMonth(date.getMonth() + 1);   
+    renderCalendar();
+})
+
+renderCalendar()
